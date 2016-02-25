76a1e117f664b624997dbc6a2352184a7680ab01
赶路者 <li5jun@126.com>
2016-02-25 14:08:41 +0800

    私有协议

---
+++ src/main/java/com/wuma/netty/privateprotocol/codec/MarshallingDecoder.java
@@ -0,0 +1,34 @@
+package com.wuma.netty.privateprotocol.codec;
+
+import io.netty.buffer.ByteBuf;
+import org.jboss.marshalling.ByteInput;
+import org.jboss.marshalling.Unmarshaller;
+
+import java.io.IOException;
+
+/**
+ * Created by liwujun
+ * on 2016/2/25 at 11:37
+ */
+public class MarshallingDecoder {
+    private final Unmarshaller unmarshaller;
+
+    public MarshallingDecoder() throws IOException {
+        unmarshaller = MarshallingCodecFactory.buildUnMarshalling();
+    }
+
+    protected Object decode(ByteBuf in) throws Exception {
+        int objectSize = in.readInt();
+        ByteBuf buf = in.slice(in.readerIndex(), objectSize);
+        ByteInput input = new ChannelBufferByteInput(buf);
+        try {
+            unmarshaller.start(input);
+            Object obj = unmarshaller.readObject();
+            unmarshaller.finish();
+            in.readerIndex(in.readerIndex() + objectSize);
+            return obj;
+        } finally {
+            unmarshaller.close();
+        }
+    }
+}
