d6f40b230455cb0ee7102a9ea51d501a1bb32221
赶路者 <li@netease>
2016-07-20 16:05:10 +0800

    找到客户端收不到信息的原因了

--- src/main/java/com/wuma/file/trans/FileClientHandler.java
+++ src/main/java/com/wuma/file/trans/FileClientHandler.java
@@ -19,6 +19,7 @@ public class FileClientHandler extends SimpleChannelInboundHandler<ByteBuf> {
         System.out.println("FileClient conn a server");
         ctx.write(Unpooled.copiedBuffer("HELO,I'm a client", CharsetUtil.UTF_8));
         System.out.println("FileClient write complete");
+        ctx.flush();
     }
 
     @Override
@@ -28,15 +29,17 @@ public class FileClientHandler extends SimpleChannelInboundHandler<ByteBuf> {
                 msg.readBytes(msg.readableBytes())));
     }
 
+
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
         cause.printStackTrace();
         ctx.close();
     }
-    public static void main(String[] args){
-        Charset utf8=Charset.forName("UTF-8");
-        ByteBuf buf=Unpooled.copiedBuffer("Netty in Action rocks!",utf8);
-        ByteBuf sliced=buf.slice(0,14);
-        ByteBuf copy=buf.copy(0,14);
+
+    public static void main(String[] args) {
+        Charset utf8 = Charset.forName("UTF-8");
+        ByteBuf buf = Unpooled.copiedBuffer("Netty in Action rocks!", utf8);
+        ByteBuf sliced = buf.slice(0, 14);
+        ByteBuf copy = buf.copy(0, 14);
         System.out.println(buf.toString(utf8));
         System.out.println(sliced.toString(utf8));
         System.out.println(copy.toString(utf8));
--- src/main/java/com/wuma/file/trans/FileTransServer.java
+++ src/main/java/com/wuma/file/trans/FileTransServer.java
@@ -1,12 +1,9 @@
 package com.wuma.file.trans;
 
-import io.netty.bootstrap.Bootstrap;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
-import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
 
@@ -18,13 +15,13 @@ public class FileTransServer {
     static final int PORT = Integer.parseInt("8023");
 
     public static void main(String[] args) throws Exception {
-//        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
         EventLoopGroup workerGroup = new NioEventLoopGroup();
         try {
             ServerBootstrap b = new ServerBootstrap();
-            b.group(workerGroup).channel(NioServerSocketChannel.class)
-//                    .handler(new LoggingHandler(LogLevel.INFO))
-//                    .option(ChannelOption.SO_BACKLOG, 100)
+            b.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .option(ChannelOption.SO_BACKLOG, 100)
                     .childHandler(new ChannelInitializer<Channel>() {
                         @Override
                         protected void initChannel(Channel ch) throws Exception {
