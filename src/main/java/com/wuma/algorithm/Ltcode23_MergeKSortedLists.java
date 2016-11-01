2ee8166d1d7c966b9b186a7d5fe59f8892f7bf0b
赶路者 <li@netease>
2016-11-01 14:53:53 +0800

    leetcode 23

---
+++ src/main/java/com/wuma/algorithm/Ltcode23_MergeKSortedLists.java
@@ -0,0 +1,67 @@
+package com.wuma.algorithm;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * Created by liwujun
+ * on 2016/10/31 at 10:16
+ * Merge k sorted linked lists
+ * and return it as one sorted list.
+ * Analyze and describe its complexity.
+ */
+public class Ltcode23_MergeKSortedLists {
+
+    /**
+     * Definition for singly-linked list.
+     **/
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode mergeKLists(ListNode[] lists) {
+
+        int k = lists.length;
+        if (k == 0) {
+            return null;
+        }
+        if (k == 1) {
+            return lists[0];
+        }
+        PriorityQueue<ListNode> heap = new PriorityQueue<ListNode>(2,
+                new Comparator<ListNode>() {
+                    public int compare(ListNode o1, ListNode o2) {
+                        return o1.val - o2.val;
+                    }
+                });
+        for (ListNode node : lists) {
+            if (node != null) {
+                heap.add(node);
+            }
+        }
+        ListNode head = null;
+        ListNode cur = null;
+        while (!heap.isEmpty()) {
+            ListNode node = heap.poll();
+            if (node.next != null) {
+                heap.add(node.next);
+            }
+            if (head == null) {
+                head = node;
+                cur = node;
+            } else {
+                cur.next = node;
+                cur = cur.next;
+            }
+        }
+        return head;
+    }
+    public static void main(String[] args){
+
+    }
+}
---
+++ src/main/java/com/wuma/algorithm/Ltcode8_StringtoInteger.java
@@ -0,0 +1,30 @@
+package com.wuma.algorithm;
+
+/**
+ * Created by liwujun
+ * on 2016/10/28 at 11:21
+ * Implement atoi to convert a string to an integer.
+ * Hint: Carefully consider all possible input cases.
+ * If you want a challenge, please do not see below and ask yourself
+ * what are the possible input cases.
+ * Notes: It is intended for this problem to be specified vaguely
+ * (ie, no given input specs). You are responsible to gather all the input requirements up front.
+ * Update (2015-02-10):
+ * The signature of the C++ function had been updated.
+ * If you still see your function signature accepts a const char * argument,
+ * please click the reload button  to reset your code definition.
+ * spoilers alert... click to show requirements for atoi.
+ * link:https://leetcode.com/problems/string-to-integer-atoi/
+ */
+public class Ltcode8_StringtoInteger {
+    public static int myAtoi(String str) {
+        char[] chars = str.toCharArray();
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(myAtoi(""));
+    }
+}
