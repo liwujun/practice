31371f75efaef7bf108121d1923c77c1a01966e2
赶路者 <li@netease>
2016-11-07 17:56:16 +0800

    two stack

---
+++ src/main/java/com/wuma/algorithm/Ltcode140_WordBreakII.java
@@ -0,0 +1,23 @@
+package com.wuma.algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Created by liwujun
+ * on 2016/11/7 at 17:55
+ * Given a string s and a dictionary of words dict,
+ * add spaces in s to construct a sentence
+ * where each word is a valid dictionary word.
+ * Return all such possible sentences.
+ * For example, given
+ * s = "catsanddog",
+ * dict = ["cat", "cats", "and", "sand", "dog"].
+ * A solution is ["cats and dog", "cat sand dog"].
+ */
+public class Ltcode140_WordBreakII {
+    public List<String> wordBreak(String s, Set<String> wordDict) {
+        return new ArrayList<String>();
+    }
+}
