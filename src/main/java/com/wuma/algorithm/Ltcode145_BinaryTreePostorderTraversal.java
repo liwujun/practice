209feaf70e06ff9d083b8f2a9cdb49cfe1f5e7a3
赶路者 <li@netease>
2016-11-07 17:26:17 +0800

    binary_post_order_traversal

---
+++ src/main/java/com/wuma/algorithm/Ltcode145_BinaryTreePostorderTraversal.java
@@ -0,0 +1,42 @@
+package com.wuma.algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by liwujun
+ * on 2016/11/7 at 16:31
+ * Given a binary tree, return the postorder traversal of its nodes' values.
+ * <p/>
+ * For example:
+ * Given binary tree {1,#,2,3},
+ * 1
+ * \
+ * 2
+ * /
+ * 3
+ * return [3,2,1].
+ * <p/>
+ * Note: Recursive solution is trivial, could you do it iteratively?
+ * link: https://leetcode.com/problems/binary-tree-postorder-traversal/
+ */
+public class Ltcode145_BinaryTreePostorderTraversal {
+    /**
+     * Definition for a binary tree node.
+     */
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+
+        List<Integer> list = new ArrayList<Integer>();
+        return list;
+    }
+}
