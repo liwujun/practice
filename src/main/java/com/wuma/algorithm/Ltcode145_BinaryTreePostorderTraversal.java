499515fd45446f25bd26accba40d81c7899a3d61
赶路者 <li@netease>
2016-11-07 17:48:58 +0800

    binary_post_order_traversal

--- src/main/java/com/wuma/algorithm/Ltcode145_BinaryTreePostorderTraversal.java
+++ src/main/java/com/wuma/algorithm/Ltcode145_BinaryTreePostorderTraversal.java
@@ -36,12 +36,37 @@ public class Ltcode145_BinaryTreePostorderTraversal {
 
     public static List<Integer> postorderTraversal(TreeNode root) {
 
+        if (root == null) {
+            return new ArrayList<Integer>();
+        }
         List<Integer> list = new ArrayList<Integer>();
+
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+        List<Integer> left_int = postorderTraversal(left);
+        List<Integer> right_int = postorderTraversal(right);
+        if (left_int != null) {
+            list.addAll(left_int);
+        }
+        if (right_int != null) {
+            list.addAll(right_int);
+        }
+        int value = root.val;
+        list.add(value);
         return list;
     }
 
+
     public static void main(String[] args) {
+        TreeNode right = new TreeNode(2);
+        TreeNode left = new TreeNode(3);
+        right.left = left;
+
         TreeNode treeNode = new TreeNode(1);
-        postorderTraversal(treeNode);
+        treeNode.right = right;
+        List<Integer> list = postorderTraversal(treeNode);
+        for (Integer integer : list) {
+            System.out.println(integer);
+        }
     }
 }
