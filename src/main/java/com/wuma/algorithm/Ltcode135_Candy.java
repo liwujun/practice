2834ac01ef7464b9e875636c7b79b94601ebe869
赶路者 <li@netease>
2016-11-18 12:58:55 +0800

    qsort

---
+++ src/main/java/com/wuma/algorithm/Ltcode135_Candy.java
@@ -0,0 +1,19 @@
+package com.wuma.algorithm;
+
+/**
+ * Created by liwujun
+ * on 2016/11/18 at 12:32
+ * There are N children standing in a line. Each child is assigned a rating value.
+
+ You are giving candies to these children subjected to the following requirements:
+
+ Each child must have at least one candy.
+ Children with a higher rating get more candies than their neighbors.
+ What is the minimum candies you must give?
+ */
+public class Ltcode135_Candy {
+    public int candy(int[] ratings) {
+
+        return 0;
+    }
+}
---
+++ src/main/java/com/wuma/algorithm/Ltcode214_ShortestPalindrome.java
@@ -0,0 +1,21 @@
+package com.wuma.algorithm;
+
+/**
+ * Created by liwujun
+ * on 2016/11/18 at 12:34
+ * Given a string S, you are allowed to
+ * convert it to a palindrome by adding characters
+ * in front of it. Find and return the shortest
+ * palindrome you can find by performing this transformation.
+ For example:
+ Given "aacecaaa", return "aaacecaaa".
+ Given "abcd", return "dcbabcd".
+ Credits:
+ Special thanks to @ifanchu for adding this problem and creating all test cases.
+ */
+public class Ltcode214_ShortestPalindrome {
+    public String shortestPalindrome(String s) {
+        return "";
+    }
+
+}
