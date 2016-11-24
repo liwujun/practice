b05b98c32041b7e4686aba38e110d77af85bd02c
赶路者 <li@netease>
2016-11-24 13:47:27 +0800

    加入正确解法Trie树

--- src/main/java/com/wuma/algorithm/Ltcode212_WordSearchII.java
+++ src/main/java/com/wuma/algorithm/Ltcode212_WordSearchII.java
@@ -1,7 +1,6 @@
 package com.wuma.algorithm;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by liwujun
@@ -25,6 +24,107 @@ import java.util.List;
  * You may assume that all inputs are consist of lowercase letters a-z.
  */
 public class Ltcode212_WordSearchII {
+    class TrieNode {
+        char c;
+        boolean leaf;
+        HashMap<Character, TrieNode> children = new HashMap<Character, TrieNode>();
+
+        public TrieNode(char c) {
+            this.c = c;
+        }
+
+        public TrieNode() {
+        }
+    }
+
+    class Trie {
+        private TrieNode root;
+
+        public Trie() {
+            root = new TrieNode();
+        }
+
+        //insert a word into trie
+        public void insert(String word) {
+            Map<Character, TrieNode> children = root.children;
+            for (int i = 0; i < word.length(); i++) {
+                char c = word.charAt(i);
+                TrieNode tn;
+                if (children.containsKey(c)) {
+                    tn = children.get(c);
+                } else {
+                    tn = new TrieNode(c);
+                    children.put(c, tn);
+                }
+                children = tn.children;
+                if (i == word.length() - 1)
+                    tn.leaf = true;
+            }
+
+        }
+
+        // Returns if the word is in the trie.
+        public boolean search(String word) {
+            TrieNode t = searchNode(word);
+            return t != null && t.leaf;
+        }
+
+        public boolean startsWith(String prefix) {
+            return searchNode(prefix) != null;
+        }
+
+        private TrieNode searchNode(String word) {
+            Map<Character, TrieNode> children = root.children;
+            TrieNode t = null;
+            for (int i = 0; i < word.length(); i++) {
+                char c = word.charAt(i);
+                if (!children.containsKey(c)) return null;
+                t = children.get(c);
+                children = t.children;
+            }
+            return t;
+        }
+    }
+
+    public List<String> findWordsds(char[][] board, String[] words) {
+        Set<String> res = new HashSet<String>();
+        if (board == null || words == null || board.length == 0 || words.length == 0)
+            return new ArrayList<String>(res);
+        boolean[][] visited = new boolean[board.length][board[0].length];
+
+        Trie trie = new Trie();
+        for (String word : words) {
+            trie.insert(word);
+        }
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                search(board, visited, trie, i, j, new StringBuilder(), res);
+            }
+        }
+        return new ArrayList<String>(res);
+    }
+
+    private void search(char[][] board, boolean[][] visited, Trie trie,
+                        int i, int j, StringBuilder sb, Set<String> res) {
+        if (i < 0 || i > board.length - 1 || j < 0 || j > board[0].length - 1 || visited[i][j])
+            return;
+        sb.append(board[i][j]);
+        String s = sb.toString();
+        visited[i][j] = true;
+        if (trie.startsWith(s)) {
+            if (trie.search(s))
+                res.add(s);
+
+            search(board, visited, trie, i - 1, j, sb, res);
+            search(board, visited, trie, i + 1, j, sb, res);
+            search(board, visited, trie, i, j - 1, sb, res);
+            search(board, visited, trie, i, j + 1, sb, res);
+        }
+        sb.deleteCharAt(sb.length() - 1);
+        visited[i][j] = false;
+    }
+
     public List<String> findWords(char[][] board, String[] words) {
         List<String> result = new ArrayList<String>();
         int heigth = board.length;
@@ -146,9 +246,17 @@ public class Ltcode212_WordSearchII {
         long start_time = System.currentTimeMillis();
         List<String> ll = ws.findWords(bbbb, wwww);
         long end_time = System.currentTimeMillis();
-        System.out.println("time elapsed:" + (end_time - start_time) / 1000 + "s");
+        System.out.println("time elapsed:" + (end_time - start_time) + "ms");
+        long start_time_ci = System.currentTimeMillis();
+        List<String> jj = ws.findWordsds(bbbb, wwww);
+        long end_time_ci = System.currentTimeMillis();
+        System.out.println("time elapsed compared:" + (end_time_ci - start_time_ci) + "ms");
         for (int i = 0; i < ll.size(); i++) {
             System.out.println(ll.get(i));
+        }
+        System.out.println("jj's size:" + jj.size());
+        for (int i = 0; i < jj.size(); i++) {
+            System.out.println("--" + jj.get(i));
         }
         boolean[][] test = new boolean[3][3];
         for (int i = 0; i < 3; i++) {
