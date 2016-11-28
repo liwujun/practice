a8fa72e45fbbb6b2410f49d0b2afb4f9a2f2b769
赶路者 <li@netease>
2016-11-28 16:26:56 +0800

    word break II

---
+++ src/main/java/com/wuma/algorithm/Ltcode164_MaximumGap.java
@@ -0,0 +1,17 @@
+package com.wuma.algorithm;
+
+/**
+ * Created by liwujun
+ * on 2016/11/28 at 16:26
+ * Given an unsorted array, find the maximum
+ * difference between the successive elements in its sorted form.
+ * Try to solve it in linear time/space.
+ * Return 0 if the array contains less than 2 elements.
+ * You may assume all elements in the array
+ * are non-negative integers and fit in the 32-bit signed integer range.
+ */
+public class Ltcode164_MaximumGap {
+    public int maximumGap(int[] nums) {
+        return nums[0];
+    }
+}
