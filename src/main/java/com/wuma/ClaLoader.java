8085f3feef48f6718d8cea9a332b53e72e84a611
赶路者 <li@netease>
2017-02-08 10:10:51 +0800

    >>>

--- src/main/java/com/wuma/ClaLoader.java
+++ src/main/java/com/wuma/ClaLoader.java
@@ -7,5 +7,14 @@ package com.wuma;
 public class ClaLoader {
     //-XX:+TraceClassLoading
     //-XX:+PrintGCDetails
+    public static void main(String[] args){
+        int negative=-15;
+        int positive=15;
+        System.out.println(negative>>>1);//print 2147483640
+        System.out.println(Integer.toBinaryString(negative));    //11111111111111111111111111110001
+        System.out.println(Integer.toBinaryString(negative>>>1));//1111111111111111111111111111000
+        System.out.println(positive>>>1);//7
+        System.out.println(Integer.toBinaryString(positive>>>1));//111
+    }
 
 }
