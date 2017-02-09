312329d6246d22b251139e6e89f2e879554697ff
oraclexbw <li5jun@126.com>
2017-02-09 23:13:56 +0800

    a heap appear

---
+++ src/main/java/com/wuma/algorithm/Ltcode215_KthLargestElementinanArray.java
@@ -0,0 +1,17 @@
+package com.wuma.algorithm;
+
+/**
+ * Created by zhang on 2017/2/9.
+ * https://leetcode.com/problems/kth-largest-element-in-an-array/
+ * Find the kth largest element in an unsorted array.
+ * Note that it is the kth largest element in the sorted order,
+ * not the kth distinct element.
+
+ For example,
+ Given [3,2,1,5,6,4] and k = 2, return 5.
+
+ Note:
+ You may assume k is always valid, 1 ¡Ü k ¡Ü array's length.
+ */
+public class Ltcode215_KthLargestElementinanArray {
+}
