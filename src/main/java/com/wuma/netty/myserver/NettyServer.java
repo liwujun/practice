e50d5ffa3a103accd800c40956a4596cde9f1591
赶路者 <li@netease>
2017-02-13 17:16:40 +0800

    准备自己写一个宏大的工程

---
+++ src/main/java/com/wuma/netty/myserver/NettyServer.java
@@ -0,0 +1,30 @@
+package com.wuma.netty.myserver;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import org.apache.log4j.Logger;
+
+/**
+ * Created by liwujun
+ * on 2017/2/13 at 16:52
+ */
+public class NettyServer {
+    private static final Logger logger = Logger.getLogger(NettyServer.class);
+    private EventLoopGroup bossGroup;
+    private EventLoopGroup workerGroup;
+
+    NettyServer() {
+        bossGroup = new NioEventLoopGroup(1);
+        workerGroup = new NioEventLoopGroup();
+        ServerBootstrap b = new ServerBootstrap();
+        b.group(bossGroup, workerGroup);
+        ChannelFuture ch = b.bind(2048);
+    }
+
+    public static void main(String[] args) {
+        NettyServer ns = new NettyServer();
+
+    }
+}
