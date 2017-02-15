653acf3435af4734437bcfd7825a8d4d7cb13ea1
赶路者 <li@netease>
2017-02-15 18:10:44 +0800

    面试题

---
+++ src/main/java/com/wuma/algorithm/Ltcode_225ImplementStackusingQueues.java
@@ -0,0 +1,62 @@
+package com.wuma.algorithm;
+
+/**
+ * Created by liwujun
+ * on 2017/2/15 at 18:08
+ * Implement the following operations of a stack using queues.
+ * <p/>
+ * push(x) -- Push element x onto stack.
+ * pop() -- Removes the element on top of the stack.
+ * top() -- Get the top element.
+ * empty() -- Return whether the stack is empty.
+ * Notes:
+ * You must use only standard operations of a queue --
+ * which means only push to back, peek/pop from front, size, and is empty operations are valid.
+ * Depending on your language, queue may not be supported natively.
+ * You may simulate a queue by using a list or deque (double-ended queue),
+ * as long as you use only standard operations of a queue.
+ * You may assume that all operations are valid (for example,
+ * no pop or top operations will be called on an empty stack).
+ */
+public class Ltcode_225ImplementStackusingQueues {    /** Initialize your data structure here. */
+
+    /**
+     * Push element x onto stack.
+     */
+    public void push(int x) {
+
+    }
+
+    /**
+     * Removes the element on top of the stack and returns that element.
+     */
+    public int pop() {
+
+        return 0;
+    }
+
+    /**
+     * Get the top element.
+     */
+    public int top() {
+
+        return 1;
+    }
+
+    /**
+     * Returns whether the stack is empty.
+     */
+    public boolean empty() {
+
+        return true;
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
