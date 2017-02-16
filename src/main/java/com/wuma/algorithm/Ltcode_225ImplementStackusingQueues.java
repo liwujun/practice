9cf3f7a124fa6511fc773e70533f78da2170a649
赶路者 <li@netease>
2017-02-16 10:07:02 +0800

    终于他么的坐对了

--- src/main/java/com/wuma/algorithm/Ltcode_225ImplementStackusingQueues.java
+++ src/main/java/com/wuma/algorithm/Ltcode_225ImplementStackusingQueues.java
@@ -1,5 +1,7 @@
 package com.wuma.algorithm;
 
+import java.util.LinkedList;
+
 /**
  * Created by wuma
  * on 2017/2/15 at 18:08
@@ -18,45 +20,83 @@ package com.wuma.algorithm;
  * You may assume that all operations are valid (for example,
  * no pop or top operations will be called on an empty stack).
  */
-public class Ltcode_225ImplementStackusingQueues {    /** Initialize your data structure here. */
-
+public class Ltcode_225ImplementStackusingQueues {
     /**
-     * Push element x onto stack.
+     * Initialize your data structure here.
      */
-    public void push(int x) {
+
+    public static class MyStack {
+        LinkedList<Integer> queue1;
+        LinkedList<Integer> queue2;
+        int size;
+
+        public MyStack() {
+            queue1 = new LinkedList<Integer>();
+            queue2 = new LinkedList<Integer>();
+            size = 0;
+        }
+
+        /**
+         * Push element x onto stack.
+         */
+        public void push(int x) {
+            queue1.offer(x);
+            size++;
+        }
+
+        /**
+         * Removes the element on top of the stack and returns that element.
+         */
+        public int pop() {
+            int result = queue1.peek();
+            int count = 0;
+            if (size == 0) {
+                return result;
+            }
+            while (!queue1.isEmpty() && (size > count + 1)) {
+                queue2.offer(result = queue1.poll());
+                count++;
+            }
+            result = queue1.poll();
+            while (!queue2.isEmpty()) {
+                queue1.offer(queue2.poll());
+            }
+            size--;
+            return result;
+        }
+
+        /**
+         * Get the top element.
+         */
+        public int top() {
+            int result = queue1.peek();
+            while (!queue1.isEmpty()) {
+                queue2.offer(result = queue1.poll());
+            }
+            while (!queue2.isEmpty()) {
+                queue1.offer(queue2.poll());
+            }
+            return result;
+        }
+
+        /**
+         * Returns whether the stack is empty.
+         */
+        public boolean empty() {
+
+            return queue1.isEmpty();
+        }
+    }
+
+    public static void main(String args[]) {
+
+        MyStack obj = new MyStack();
+        obj.push(1);
+        int param_2 = obj.pop();
+        int param_3 = obj.top();
+        boolean param_4 = obj.empty();
 
     }
 
-    /**
-     * Removes the element on top of the stack and returns that element.
-     */
-    public int pop() {
-
-        return 0;
-    }
-
-    /**
-     * Get the top element.
-     */
-    public int top() {
-
-        return 1;
-    }
-
-    /**
-     * Returns whether the stack is empty.
-     */
-    public boolean empty() {
-
-        return true;
-    }
 }
 
-/**
- * Your MyStack object will be instantiated and called as such:
- * MyStack obj = new MyStack();
- * obj.push(x);
- * int param_2 = obj.pop();
- * int param_3 = obj.top();
- * boolean param_4 = obj.empty();
- */
