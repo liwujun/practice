95fc3cecf0e7a9982d983cb835b2b670552a7626
li's MacBook <lwj@kuba.bz>
2017-02-19 15:24:38 +0800

    in order traversal

--- src/main/java/com/wuma/algorithm/Ltcode94_BinaryTreeInorderTraversal.java
+++ src/main/java/com/wuma/algorithm/Ltcode94_BinaryTreeInorderTraversal.java
@@ -2,6 +2,7 @@ package com.wuma.algorithm;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * Created by wuma
@@ -31,6 +32,39 @@ public class Ltcode94_BinaryTreeInorderTraversal {
     }
 
     public List<Integer> inorderTraversal(TreeNode root) {
-        return new ArrayList<Integer>();
+        List<Integer> list=new ArrayList<Integer>();
+        Stack<TreeNode> s1=new Stack<TreeNode>();
+
+        traversalLeft(root,s1);
+        while (!s1.empty()){
+            TreeNode temp=s1.peek();
+
+            if (temp.right!=null){
+                list.add(s1.pop().val);
+                traversalLeft(temp.right,s1);
+            }else {
+                list.add(s1.pop().val);
+            }
+        }
+        return list;
+    }
+
+    public void traversalLeft(TreeNode tn,Stack s){
+        while (tn!=null){
+         s.push(tn);
+            tn=tn.left;
+        }
+    }
+
+    public static void main(String args[]){
+        TreeNode tn1=new TreeNode(1);
+        TreeNode tn2=new TreeNode(2);
+        TreeNode tn3=new TreeNode(3);
+        tn1.right=tn2;
+        tn2.left=tn3;
+        Ltcode94_BinaryTreeInorderTraversal lc=new Ltcode94_BinaryTreeInorderTraversal();
+        List<Integer> list=lc.inorderTraversal(tn1);
+        System.out.println(list.toArray());
+
     }
 }
