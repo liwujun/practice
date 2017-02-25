8d0f49b2ed456af34fd0deb88d0bab52753fc0a7
li's MacBook <lwj@kuba.bz>
2017-02-25 09:44:29 +0800

    Integer.highestOnBit

--- src/main/java/com/wuma/diaosi/HeapSort.java
+++ src/main/java/com/wuma/diaosi/HeapSort.java
@@ -1,5 +1,8 @@
 package com.wuma.diaosi;
 
+import org.apache.hadoop.util.IndexedSortable;
+import org.apache.hadoop.util.Progressable;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -73,6 +76,55 @@ public class HeapSort {
         return deleteoneheap;
     }
 
+    /**
+     * come from hadoop-common project
+     * @param s
+     * @param b
+     * @param i
+     * @param N
+     */
+    private static void downHeap(final IndexedSortable s, final int b,
+                                 int i, final int N) {
+        for (int idx = i << 1; idx < N; idx = i << 1) {
+            if (idx + 1 < N && s.compare(b + idx, b + idx + 1) < 0) {
+                if (s.compare(b + i, b + idx + 1) < 0) {
+                    s.swap(b + i, b + idx + 1);
+                } else return;
+                i = idx + 1;
+            } else if (s.compare(b + i, b + idx) < 0) {
+                s.swap(b + i, b + idx);
+                i = idx;
+            } else return;
+        }
+    }
+
+    /**
+     * Sort the given range of items using heap sort.
+     * {@inheritDoc}
+     */
+
+    public void sort(IndexedSortable s, int p, int r) {
+        sort(s, p, r, null);
+    }
+
+    public void sort(final IndexedSortable s, final int p, final int r,
+                     final Progressable rep) {
+        final int N = r - p;
+        // build heap w/ reverse comparator, then write in-place from end
+        final int t = Integer.highestOneBit(N);
+        for (int i = t; i > 1; i >>>= 1) {
+            for (int j = i >>> 1; j < i; ++j) {
+                downHeap(s, p-1, j, N + 1);
+            }
+            if (null != rep) {
+                rep.progress();
+            }
+        }
+        for (int i = r - 1; i > p; --i) {
+            s.swap(p, i);
+            downHeap(s, p - 1, 1, i - p + 1);
+        }
+    }
     public static void main(String[] args) throws Exception {
         int[] xx = new HeapSort().hs(new BufferedReader(new InputStreamReader(System.in)), System.out);
         for (int x : xx) {
@@ -83,5 +135,6 @@ public class HeapSort {
             System.out.print(xx[0]+" ");
             xx = adjustMinHeap(xx);
         }
+        System.out.println("---"+Integer.highestOneBit(100));
     }
 }
