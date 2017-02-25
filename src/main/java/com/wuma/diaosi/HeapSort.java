25acd96cd20e719ee48004c7e5e2e6a4bdb20754
li's MacBook <lwj@kuba.bz>
2017-02-25 11:17:42 +0800

    Integer.highestOnBit

--- src/main/java/com/wuma/diaosi/HeapSort.java
+++ src/main/java/com/wuma/diaosi/HeapSort.java
@@ -1,12 +1,9 @@
 package com.wuma.diaosi;
-
-import org.apache.hadoop.util.IndexedSortable;
-import org.apache.hadoop.util.Progressable;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
+import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.Stack;
 
@@ -83,7 +80,7 @@ public class HeapSort {
      * @param i
      * @param N
      */
-    private static void downHeap(final IndexedSortable s, final int b,
+    private static void downHeap(final sortable s, final int b,
                                  int i, final int N) {
         for (int idx = i << 1; idx < N; idx = i << 1) {
             if (idx + 1 < N && s.compare(b + idx, b + idx + 1) < 0) {
@@ -102,13 +99,7 @@ public class HeapSort {
      * Sort the given range of items using heap sort.
      * {@inheritDoc}
      */
-
-    public void sort(IndexedSortable s, int p, int r) {
-        sort(s, p, r, null);
-    }
-
-    public void sort(final IndexedSortable s, final int p, final int r,
-                     final Progressable rep) {
+    public void sort(final sortable s, final int p, final int r) {
         final int N = r - p;
         // build heap w/ reverse comparator, then write in-place from end
         final int t = Integer.highestOneBit(N);
@@ -116,15 +107,38 @@ public class HeapSort {
             for (int j = i >>> 1; j < i; ++j) {
                 downHeap(s, p-1, j, N + 1);
             }
-            if (null != rep) {
-                rep.progress();
-            }
+
         }
         for (int i = r - 1; i > p; --i) {
             s.swap(p, i);
             downHeap(s, p - 1, 1, i - p + 1);
         }
     }
+
+    interface sortable{
+        public void swap(int i,int j);
+        public int compare(int i,int j);
+    }
+    class Seq implements sortable{
+        int size;
+        int[] num;
+        public Seq(int[] src){
+            size=src.length;
+            num=new int[size];
+            num=Arrays.copyOf(src,size);
+        }
+
+        public void swap(int i,int j) {
+            int temp=num[i];
+            num[i]=num[j];
+            num[j]=temp;
+        }
+
+        public int compare(int i,int j) {
+            return num[i]-num[j];
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         int[] xx = new HeapSort().hs(new BufferedReader(new InputStreamReader(System.in)), System.out);
         for (int x : xx) {
