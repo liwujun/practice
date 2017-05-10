7b5e0e8e1fd3352ddba29947013c3e36ab704e73
wujun.li <wujun.li@corp.elong.com>
2017-05-10 09:43:21 +0800

    发布新报

--- src/main/java/com/wuma/crawler/ComplicateClass.java
+++ src/main/java/com/wuma/crawler/ComplicateClass.java
@@ -5,11 +5,24 @@ package com.wuma.crawler;
  */
 public class ComplicateClass<Req, Resp> implements BaseClass<Req, Resp> {
 
+    Req req;
     public Class<Resp> getResp() {
         return null;
     }
 
     public Req getRequest() {
+        return null;
+    }
+    public <T> void showElemets(T... args){
+//        T[] array=new T[10];
+        //Type parameter cannot be instantiated directly
+    }
+
+    public void constructorF(){
+//        req=new Req();
+    }
+    //<T>用来声明这个函数与泛型有关
+    public <T> T showElemets(){
         return null;
     }
 }
