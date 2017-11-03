ee6c1576676fd6222fa2746d112e1c96c9bc0ea3
wujun.li <wujun.li@corp.elong.com>
2017-11-03 10:50:52 +0800

    awt

--- src/main/java/com/wuma/StrTest.java
+++ src/main/java/com/wuma/StrTest.java
@@ -4,6 +4,7 @@ import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by user on 17/4/12.
@@ -11,16 +12,20 @@ import java.util.List;
 public class StrTest {
 
     public static void main(String[] args) throws UnsupportedEncodingException {
-        String str="http://ssjj.com?apx=jj&tcketn=22222|8888|99999|&x=j";
-        System.out.println(str);
-        System.out.println(str.replace("|&","&"));
-        System.out.println(str.replace("|", URLEncoder.encode("|","utf8")));
-        System.out.println(str.substring(0,str.length()-1));
-        List<Integer> list=new ArrayList<>();
-        for (int i=0;i<100;i++){
-            list.add(i);
+//        String str="http://ssjj.com?apx=jj&tcketn=22222|8888|99999|&x=j";
+//        System.out.println(str);
+//        System.out.println(str.replace("|&","&"));
+//        System.out.println(str.replace("|", URLEncoder.encode("|","utf8")));
+//        System.out.println(str.substring(0,str.length()-1));
+//        List<Integer> list=new ArrayList<Integer>();
+//        for (int i=0;i<100;i++){
+//            list.add(i);
+//        }
+//        list=list.subList(0,7);
+//        System.out.println(list.size());
+        List<String> map=null;
+        for (String str:map){
+            System.out.println(str);
         }
-        list=list.subList(0,7);
-        System.out.println(list.size());
     }
 }
